4d82922eccbcb09cdc20d4a2377771e29169b344
Nuullity <marcos.venancio058@gmail.com>
2023-01-27 03:23:48 -0300

    Initial commit

---
+++ Auto_Rpg_Roll/Personagem.cs
@@ -0,0 +1,203 @@
+﻿using System;
+
+
+namespace Auto_Rpg_Roll
+{
+    class Personagem
+    {
+
+
+
+
+
+
+        public string Nome;
+        public int Idade;
+        public int Classe;
+        public int Raca;
+        public int PontosVida;
+        public int BonusRaca;
+        public int BonusClasse;
+        public int BonusProeficiencia;
+        public int C, E, I, F, A, R ;
+
+
+
+
+        public int PontosRaca()
+        {
+
+            switch (Raca)
+            {
+
+                case 1:
+
+                   R = R + 2;
+                   E = E + 1;
+                    break;
+                case 2:
+
+
+                    F = F + 2;
+                    R = R + 2;
+                    break;
+                case 3:
+
+
+                    A = A + 2;
+                    I = I + 1;
+                    
+
+
+                    break;
+                case 4:
+
+
+
+                     A = A + 2;
+
+                     E = E + 1;
+
+
+
+                    break;
+                case 5:
+
+
+                     A = A + 2;
+
+                     C = C + 1;
+                    break;
+
+                case 6:
+
+
+                     A = A + 2;
+
+                     C = C + 1;
+
+                    break;
+                case 7:
+
+
+                    A = A + 2;
+
+                     R = R + 1;
+
+
+
+                    break;
+                case 8:
+
+
+                     C = C + 1;
+                     E = E + 1;
+                     I = I + 1;
+                     F = F + 1;
+                     A = A + 1;
+                     R = R + 1;
+
+
+
+                    break;
+                case 9:
+
+
+                    F = F + 2; 
+                    C = C + 1;
+
+                     
+
+
+                    break;
+                case 10:
+
+
+                     A = A + 1;
+                     I = I + 2;
+
+
+                    break;
+                case 11:
+
+
+                     R = R + 1;
+                     I = I + 2;
+
+
+
+                    break;
+                case 12:
+
+                     R = R + 2;
+                     R = R + 2;
+
+
+
+                    break;
+                case 13:
+
+                     F = F + 2;
+
+                    // BNE = Bonus Meio elfo
+                    Console.WriteLine("Escolha 1  entre C E I F A R para aumentar em 1 ponto:");
+                    char BNE1 = Char.Parse(Console.ReadLine());
+                    Console.WriteLine("Escolha 1  entre C E I F A R para aumentar em 1 ponto:");
+                    char BNE2 = Char.Parse(Console.ReadLine());
+                    // eu odeio meio elfos
+
+                    if (BNE1 == 'C' || BNE1 == 'c')      { C = C + 1; }
+                    else if (BNE1 == 'E' || BNE1 == 'e') { E = E + 1; }
+                    else if (BNE1 == 'I' || BNE1 == 'i') { I = I + 1; }
+                    else if (BNE1 == 'F' || BNE1 == 'f') { F = F + 1; }
+                    else if (BNE1 == 'A' || BNE1 == 'a') { A = A + 1; }
+                    else if (BNE1 == 'R' || BNE1 == 'r') { R = R + 1; }
+
+                    if (BNE2 == 'C' || BNE2 == 'c') { C = C + 1; }
+                    else if (BNE2 == 'E' || BNE2 == 'e') { E = E + 1; }
+                    else if (BNE2 == 'I' || BNE2 == 'i') { I = I + 1; }
+                    else if (BNE2 == 'F' || BNE2 == 'f') { F = F + 1; }
+                    else if (BNE2 == 'A' || BNE2 == 'a') { A = A + 1; }
+                    else if (BNE2 == 'R' || BNE2 == 'r') { R = R + 1; }
+
+
+                    break;
+
+
+
+                case 14:
+
+                     I = I + 1;
+                     C = C + 2;
+
+
+                    break;
+                default:
+                    Console.WriteLine("raça invalida");
+                    Raca = -1;
+                    break;
+
+
+
+
+
+
+
+
+            }
+            
+             return 0; 
+        }
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
---
+++ Auto_Rpg_Roll/Program.cs
@@ -0,0 +1,142 @@
+﻿namespace Auto_Rpg_Roll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char genero = 'a';
+            Personagem personagem = new Personagem();
+            personagem.Raca = -1;
+
+            /*
+            Console.WriteLine(" -----------------------------------------------------");
+            Console.WriteLine(" Nome:            Idade:             Classe:          ");
+            Console.WriteLine(" Raça:            Pontos de Vida:                     ");
+            Console.WriteLine("                                                      ");
+            Console.WriteLine(" Suas Proeficiencias:                                 ");
+            Console.WriteLine("  >                                                   ");
+            Console.WriteLine("  >                                                   ");
+            Console.WriteLine("  >                                                   ");
+            Console.WriteLine("                                                      ");
+            Console.WriteLine(" Carisma:                                             ");
+            Console.WriteLine(" Esperteza:                                           ");
+            Console.WriteLine(" Inteligência:                                        ");
+            Console.WriteLine(" Força:                                               ");
+            Console.WriteLine(" Agilidade:                                           ");
+            Console.WriteLine(" Resistencia:                                         ");
+            Console.WriteLine("                                                      ");
+            Console.WriteLine(" -----------------------------------------------------");
+            */
+
+
+
+            // fazer com que o programa apenas prossiga com um genero valido
+            while (genero != 'M' && genero != 'm' && genero != 'N' && genero != 'n' && genero != 'F' && genero != 'f')
+            {
+
+
+                Console.Write("Olá Qual seu genero ? ");
+                Console.WriteLine("|M| |N| |F|");
+                genero = char.Parse(Console.ReadLine());
+                Console.Clear();
+
+
+            }
+            if (genero == 'F' || genero == 'f')
+            {
+
+
+                Console.WriteLine("Olá forasteira, esta pronta para CEIFAR algumas almas?");
+            }
+            else if (genero == 'N' || genero == 'n')
+            {
+
+                Console.WriteLine("Olá forasteire, esta pronte para CEIFAR algumas almas?");
+
+            }
+            else if (genero == 'M' || genero == 'm')
+            {
+
+                Console.WriteLine("Olá forasteiro, esta pronto para CEIFAR algumas almas?");
+            }
+
+
+            Console.Write("Qual o nome de seu Personagem ?");
+            personagem.Nome = Console.ReadLine();
+
+
+
+            Console.Write("Qual será sua idade ?");
+            personagem.Idade = int.Parse(Console.ReadLine());
+
+
+
+
+            // Raça e classe são jogados na Class personagem e la é feito os processos
+            //definir raça
+
+            while (personagem.Raca == -1)
+            {
+                Console.Clear();
+                Console.WriteLine("---------------------------------------------------------------");
+                Console.WriteLine("|                    >Escolha sua Raça<                       |");
+                Console.WriteLine("|                                                             |");
+                Console.WriteLine("|(1) - Anão da Colina               ( 8) - Humano             |");
+                Console.WriteLine("|(2) - Anão da Montanha             ( 9) - Draconato          |");
+                Console.WriteLine("|(3) - Alto Elfo                    (10) - Gnomo da Floresta  |");
+                Console.WriteLine("|(4) - Elfo da Floresta             (11) - Gnomo das Rochas   |");
+                Console.WriteLine("|(5) - Elfo Negro                   (12) - Meio Elfo          |");
+                Console.WriteLine("|(6) - Halfling dos Pés Leves       (13) - Meio Orc           |");
+                Console.WriteLine("|(7) - Halfling Robusto             (14) - Tiefling           |");
+                Console.WriteLine("---------------------------------------------------------------");
+
+
+
+                personagem.Raca = int.Parse(Console.ReadLine());
+                personagem.PontosRaca();
+
+               
+
+            }
+
+
+            // CONTINUAR A APRTIR DAQUI, CRIAÇÃO DE SISTEMA DE CLASSES DE FORMA QUE LIMITE AS PROEFICIENCIAS DISPONIVEIS ASSIM COMO CALCULE A VIDA DO PLAYER, NECESSARIO VER COMANDO PARA GERAR NUMERO ALEATORIO EM UM RANGE)
+            //definir classe 
+
+
+            Console.WriteLine("---------------------------------------------------------------");
+            Console.WriteLine("|                    >Escolha sua Classe<                     |");
+            Console.WriteLine("|                                                             |");
+            Console.WriteLine("|(1) - Bárbaro                      ( 7) - Guerreiro          |");
+            Console.WriteLine("|(2) - Bardo                        ( 8) - Ladino             |");
+            Console.WriteLine("|(3) - Bruxo                        ( 9) - Mago               |");
+            Console.WriteLine("|(4) - Clérigo                      (10) - Monge              |");
+            Console.WriteLine("|(5) - Druida                       (11) - Paladino           |");
+            Console.WriteLine("|(6) - Feiticeiro                   (12) - Patrulheiro        |");
+            Console.WriteLine("---------------------------------------------------------------");
+            Console.WriteLine("Work In Progress................");
+
+
+
+
+            personagem.Classe = int.Parse(Console.ReadLine());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
