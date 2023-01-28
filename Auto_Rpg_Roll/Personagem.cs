d5f7a4094a38e153055e2ece2ab5d7e193d25064
Nuullity <marcos.venancio058@gmail.com>
2023-01-27 23:30:02 -0300

    .

--- Auto_Rpg_Roll/Personagem.cs
+++ Auto_Rpg_Roll/Personagem.cs
@@ -16,10 +16,19 @@ namespace Auto_Rpg_Roll
         public int Classe;
         public int Raca;
         public int PontosVida;
-        public int BonusRaca;
-        public int BonusClasse;
-        public int BonusProeficiencia;
-        public int C, E, I, F, A, R ;
+        public int C, E, I, F, A, R;
+        public int[] Pericia;
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -31,122 +40,66 @@ namespace Auto_Rpg_Roll
             {
 
                 case 1:
-
-                   R = R + 2;
-                   E = E + 1;
+                    R = R + 2;
+                    E = E + 1;
                     break;
                 case 2:
-
-
                     F = F + 2;
                     R = R + 2;
                     break;
                 case 3:
-
-
                     A = A + 2;
                     I = I + 1;
-                    
-
-
                     break;
                 case 4:
-
-
-
-                     A = A + 2;
-
-                     E = E + 1;
-
-
-
+                    A = A + 2;
+                    E = E + 1;
                     break;
                 case 5:
-
-
-                     A = A + 2;
-
-                     C = C + 1;
+                    A = A + 2;
+                    C = C + 1;
                     break;
-
                 case 6:
-
-
-                     A = A + 2;
-
-                     C = C + 1;
-
+                    A = A + 2;
+                    C = C + 1;
                     break;
                 case 7:
-
-
                     A = A + 2;
-
-                     R = R + 1;
-
-
-
+                    R = R + 1;
                     break;
                 case 8:
-
-
-                     C = C + 1;
-                     E = E + 1;
-                     I = I + 1;
-                     F = F + 1;
-                     A = A + 1;
-                     R = R + 1;
-
-
-
+                    C = C + 1;
+                    E = E + 1;
+                    I = I + 1;
+                    F = F + 1;
+                    A = A + 1;
+                    R = R + 1;
                     break;
                 case 9:
-
-
-                    F = F + 2; 
+                    F = F + 2;
                     C = C + 1;
-
-                     
-
-
                     break;
                 case 10:
-
-
-                     A = A + 1;
-                     I = I + 2;
-
-
+                    A = A + 1;
+                    I = I + 2;
                     break;
                 case 11:
-
-
-                     R = R + 1;
-                     I = I + 2;
-
-
-
+                    R = R + 1;
+                    I = I + 2;
                     break;
                 case 12:
-
-                     R = R + 2;
-                     R = R + 2;
-
-
-
+                    R = R + 2;
+                    R = R + 2;
                     break;
                 case 13:
-
-                     F = F + 2;
-
+                    F = F + 2;
                     // BNE = Bonus Meio elfo
                     Console.WriteLine("Escolha 1  entre C E I F A R para aumentar em 1 ponto:");
                     char BNE1 = Char.Parse(Console.ReadLine());
                     Console.WriteLine("Escolha 1  entre C E I F A R para aumentar em 1 ponto:");
                     char BNE2 = Char.Parse(Console.ReadLine());
                     // eu odeio meio elfos
-
-                    if (BNE1 == 'C' || BNE1 == 'c')      { C = C + 1; }
+                    if (BNE1 == 'C' || BNE1 == 'c') { C = C + 1; }
                     else if (BNE1 == 'E' || BNE1 == 'e') { E = E + 1; }
                     else if (BNE1 == 'I' || BNE1 == 'i') { I = I + 1; }
                     else if (BNE1 == 'F' || BNE1 == 'f') { F = F + 1; }
@@ -159,25 +112,58 @@ namespace Auto_Rpg_Roll
                     else if (BNE2 == 'F' || BNE2 == 'f') { F = F + 1; }
                     else if (BNE2 == 'A' || BNE2 == 'a') { A = A + 1; }
                     else if (BNE2 == 'R' || BNE2 == 'r') { R = R + 1; }
-
-
                     break;
-
-
-
                 case 14:
-
-                     I = I + 1;
-                     C = C + 2;
-
-
+                    I = I + 1;
+                    C = C + 2;
                     break;
                 default:
                     Console.WriteLine("raça invalida");
                     Raca = -1;
                     break;
 
+            }
 
+
+
+
+            return 0;
+        }
+        public int PericiasClasse()
+        {
+            switch (Classe)
+            {
+
+                case 1:
+                    Console.WriteLine("Escolha uma destas perícias");
+
+                    Console.WriteLine("--------------------------------------------------------------------");
+                    Console.WriteLine("|                   >Escolha sua Pericias<                         |");
+                    Console.WriteLine("|                                                                  |");
+                    Console.WriteLine("|(1) - Atletismo                    (4) - Adestrar Animais         |");
+                    Console.WriteLine("|(2) - Intimidação                  (5) - Natureza                 |");
+                    Console.WriteLine("|(3) - Percepção                    (6) - Sobrevivencia            |");
+                    Console.WriteLine("--------------------------------------------------------------------");
+                    Pericia[0] = int.Parse(Console.ReadLine());
+                    Console.Clear();
+                    Console.WriteLine("Escolha uma destas perícias");
+                    Console.WriteLine("--------------------------------------------------------------------");
+                    Console.WriteLine("|                   >Escolha sua Pericias<                         |");
+                    Console.WriteLine("|                                                                  |");
+                    Console.WriteLine("|(1) - Atletismo                    (4) - Adestrar Animais         |");
+                    Console.WriteLine("|(2) - Intimidação                  (5) - Natureza                 |");
+                    Console.WriteLine("|(3) - Percepção                    (6) - Sobrevivencia            |");
+                    Console.WriteLine("--------------------------------------------------------------------");
+                    Pericia[1] = int.Parse(Console.ReadLine());
+                    PontosVida = 12 +
+                        // erro para não me esquecer 
+
+
+
+
+
+
+                    break;
 
 
 
@@ -185,10 +171,14 @@ namespace Auto_Rpg_Roll
 
 
             }
-            
-             return 0; 
-        }
 
+
+
+
+
+
+            return 0;
+        }
 
 
 
--- Auto_Rpg_Roll/Program.cs
+++ Auto_Rpg_Roll/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Auto_Rpg_Roll
+﻿using System.Data;
+
+namespace Auto_Rpg_Roll
 {
     internal class Program
     {
@@ -6,6 +8,9 @@
         {
 
             char genero = 'a';
+            int pontos_atributos = 27;
+            int provisorio = 0;
+
             Personagem personagem = new Personagem();
             personagem.Raca = -1;
 
@@ -62,81 +67,235 @@
             }
 
 
-            Console.Write("Qual o nome de seu Personagem ?");
+            Console.Write("Qual o nome de seu Personagem ? ");
             personagem.Nome = Console.ReadLine();
 
 
 
-            Console.Write("Qual será sua idade ?");
+            Console.Write("Qual será sua idade ? ");
             personagem.Idade = int.Parse(Console.ReadLine());
 
+            Console.Clear();
+            // fazer  atribuição de atributos, com base na funçao Atributo
+            Console.WriteLine($"Voce irá definir agr os pontos de atributos de seu personagem");
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("|          Valor Custo |    Valor Custo      |");
+            Console.WriteLine("|        (1)  8     0  | (5)  12     4       |");
+            Console.WriteLine("|        (2)  9     1  | (6)  13     5       |");
+            Console.WriteLine("|        (3)  10    2  | (7)  14     7       |");
+            Console.WriteLine("|        (4)  11    3  | (8)  15     9       |");
+            Console.WriteLine("|                                            |");
+            Console.WriteLine("|        Caso fique com Pontos negativos     |");
+            Console.WriteLine("|   o software irá se fechar automaticamente |");
+            Console.WriteLine("----------------------------------------------");
 
+            Console.Write($"Voce possui {pontos_atributos} pontos, defina o valor para o atributo Carisma: ");
+            personagem.C = provisorio = Atributo(int.Parse(Console.ReadLine()));
+            pontos_atributos = pontos_atributos - Custo(provisorio);
+            Console.Write($"Voce possui {pontos_atributos} pontos, defina o valor para o atributo Esperteza: ");
+            personagem.E = provisorio = Atributo(int.Parse(Console.ReadLine()));
+            pontos_atributos = pontos_atributos - Custo(provisorio);
 
+            Console.Write($"Voce possui {pontos_atributos} pontos, defina o valor para o atributo Inteligência: ");
+            personagem.I = provisorio = Atributo(int.Parse(Console.ReadLine()));
+            pontos_atributos = pontos_atributos - Custo(provisorio);
 
-            // Raça e classe são jogados na Class personagem e la é feito os processos
-            //definir raça
+            Console.Write($"Voce possui {pontos_atributos} pontos, defina o valor para o atributo Força: ");
+            personagem.F = provisorio = Atributo(int.Parse(Console.ReadLine()));
+            pontos_atributos = pontos_atributos - Custo(provisorio);
 
-            while (personagem.Raca == -1)
+            Console.Write($"Voce possui {pontos_atributos} pontos, defina o valor para o atributo Agilidade: ");
+            personagem.A = provisorio = Atributo(int.Parse(Console.ReadLine()));
+            pontos_atributos = pontos_atributos - Custo(provisorio);
+            Console.Write($"Voce possui {pontos_atributos} pontos, defina o valor para o atributo Resistência: ");
+            personagem.C = provisorio = Atributo(int.Parse(Console.ReadLine()));
+            pontos_atributos = pontos_atributos - Custo(provisorio);
+
+            Console.Clear();
+
+            if (pontos_atributos >= 0)
             {
-                Console.Clear();
+
+
+
+
+
+                // Raça e classe são jogados na Class personagem e la é feito os processos
+                //definir raça
+
+
+                while (personagem.Raca == -1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("---------------------------------------------------------------");
+                    Console.WriteLine("|                    >Escolha sua Raça<                       |");
+                    Console.WriteLine("|                                                             |");
+                    Console.WriteLine("|(1) - Anão da Colina               ( 8) - Humano             |");
+                    Console.WriteLine("|(2) - Anão da Montanha             ( 9) - Draconato          |");
+                    Console.WriteLine("|(3) - Alto Elfo                    (10) - Gnomo da Floresta  |");
+                    Console.WriteLine("|(4) - Elfo da Floresta             (11) - Gnomo das Rochas   |");
+                    Console.WriteLine("|(5) - Elfo Negro                   (12) - Meio Elfo          |");
+                    Console.WriteLine("|(6) - Halfling dos Pés Leves       (13) - Meio Orc           |");
+                    Console.WriteLine("|(7) - Halfling Robusto             (14) - Tiefling           |");
+                    Console.WriteLine("---------------------------------------------------------------");
+
+
+
+                    personagem.Raca = int.Parse(Console.ReadLine());
+                    personagem.PontosRaca();
+
+
+
+                }
+
+
+                // CONTINUAR A APRTIR DAQUI, CRIAÇÃO DE SISTEMA DE CLASSES DE FORMA QUE LIMITE AS PROEFICIENCIAS DISPONIVEIS ASSIM COMO CALCULE A VIDA DO PLAYER, NECESSARIO VER COMANDO PARA GERAR NUMERO ALEATORIO EM UM RANGE)
+                //definir classe 
+
+
                 Console.WriteLine("---------------------------------------------------------------");
-                Console.WriteLine("|                    >Escolha sua Raça<                       |");
+                Console.WriteLine("|                    >Escolha sua Classe<                     |");
                 Console.WriteLine("|                                                             |");
-                Console.WriteLine("|(1) - Anão da Colina               ( 8) - Humano             |");
-                Console.WriteLine("|(2) - Anão da Montanha             ( 9) - Draconato          |");
-                Console.WriteLine("|(3) - Alto Elfo                    (10) - Gnomo da Floresta  |");
-                Console.WriteLine("|(4) - Elfo da Floresta             (11) - Gnomo das Rochas   |");
-                Console.WriteLine("|(5) - Elfo Negro                   (12) - Meio Elfo          |");
-                Console.WriteLine("|(6) - Halfling dos Pés Leves       (13) - Meio Orc           |");
-                Console.WriteLine("|(7) - Halfling Robusto             (14) - Tiefling           |");
+                Console.WriteLine("|(1) - Bárbaro                      ( 7) - Guerreiro          |");
+                Console.WriteLine("|(2) - Bardo                        ( 8) - Ladino             |");
+                Console.WriteLine("|(3) - Bruxo                        ( 9) - Mago               |");
+                Console.WriteLine("|(4) - Clérigo                      (10) - Monge              |");
+                Console.WriteLine("|(5) - Druida                       (11) - Paladino           |");
+                Console.WriteLine("|(6) - Feiticeiro                   (12) - Patrulheiro        |");
                 Console.WriteLine("---------------------------------------------------------------");
+                Console.WriteLine("Work In Progress................");
 
 
 
-                personagem.Raca = int.Parse(Console.ReadLine());
-                personagem.PontosRaca();
 
-               
+                personagem.Classe = int.Parse(Console.ReadLine());
+                personagem.Classe = int.Parse(Console.ReadLine());
+                personagem.Classe = int.Parse(Console.ReadLine());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+            static int Atributo(int m)
+            {
+
+                switch (m)
+                {
+
+                    case 1:
+
+                        m = 8;
+
+
+
+
+
+                        break;
+                    case 2:
+                        m = 9;
+
+
+
+                        break;
+                    case 3:
+                        m = 10;
+
+
+                        break;
+                    case 4:
+                        m = 11;
+
+
+                        break;
+                    case 5:
+                        m = 12;
+
+                        break;
+                    case 6:
+                        m = 13;
+
+                        break;
+                    case 7:
+                        m = 14;
+
+                        break;
+                    case 8:
+                        m = 15;
+
+                        break;
+                        
+                }
+                return m;
+
+
+
+            }
+            static int Custo(int m)
+            {
+                switch (m)
+                {
+
+                    case 1:
+
+                        m = -0;
+
+
+
+
+
+                        break;
+                    case 2:
+                        m = -1;
+
+
+
+                        break;
+                    case 3:
+                        m = -2;
+
+                        break;
+                    case 4:
+                        m = -3;
+
+                        break;
+                    case 5:
+                        m = -4;
+                        break;
+                    case 6:
+
+                        m = -5;
+                        break;
+                    case 7:
+
+                        m = -7;
+                        break;
+                    case 8:
+                        m = -9; ;
+
+                        break;
+
+                }
+                return m;
+
 
             }
 
 
-            // CONTINUAR A APRTIR DAQUI, CRIAÇÃO DE SISTEMA DE CLASSES DE FORMA QUE LIMITE AS PROEFICIENCIAS DISPONIVEIS ASSIM COMO CALCULE A VIDA DO PLAYER, NECESSARIO VER COMANDO PARA GERAR NUMERO ALEATORIO EM UM RANGE)
-            //definir classe 
-
-
-            Console.WriteLine("---------------------------------------------------------------");
-            Console.WriteLine("|                    >Escolha sua Classe<                     |");
-            Console.WriteLine("|                                                             |");
-            Console.WriteLine("|(1) - Bárbaro                      ( 7) - Guerreiro          |");
-            Console.WriteLine("|(2) - Bardo                        ( 8) - Ladino             |");
-            Console.WriteLine("|(3) - Bruxo                        ( 9) - Mago               |");
-            Console.WriteLine("|(4) - Clérigo                      (10) - Monge              |");
-            Console.WriteLine("|(5) - Druida                       (11) - Paladino           |");
-            Console.WriteLine("|(6) - Feiticeiro                   (12) - Patrulheiro        |");
-            Console.WriteLine("---------------------------------------------------------------");
-            Console.WriteLine("Work In Progress................");
-
-
-
-
-            personagem.Classe = int.Parse(Console.ReadLine());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         }
-    }
+
+    }// fechar a gambiarra do if para custo de atributo
+
 }
